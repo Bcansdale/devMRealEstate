0cc6d3c92e54e171c8402c793ee7431848b47e49
Bcansdale <28786879+Bcansdale@users.noreply.github.com>
2024-09-23 14:21:06 -0600

    *Auth Controller* setup

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,28 @@
-const Reducer = () => {
-
+const initialState = {
+    userId: null,
+    otherValue: "hello"
 };
 
-export default Reducer;
+// front end components will dispatch an action object :
+// { type: "USER_AUTH", payload: userId }
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "USER_AUTH":
+            return {
+                ...state,
+                userId: action.payload,
+            };
+
+        // triggered from front end with this dispatch action object: { type: "LOGOUT" }
+        case "LOGOUT":
+            return {
+                ...state,
+                userId: null,
+            };
+
+        default:
+            return state;
+    }
+};
+
+export default reducer;
